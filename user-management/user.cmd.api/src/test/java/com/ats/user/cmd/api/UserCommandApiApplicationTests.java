829893e7930042e7a16700ef3dfea6a93075ca7a
BondarOleg98 <olegb5195@gmail.com>
2021-09-08 15:49:09 -0400

    Added a user-management service

---
+++ user-management/user.cmd.api/src/test/java/com/ats/user/cmd/api/UserCommandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.user.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCommandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
