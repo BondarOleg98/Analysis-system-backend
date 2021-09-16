c5b3291ed6a5523e5c2650db297e4b3a878243e0
BondarOleg98 <olegb5195@gmail.com>
2021-09-17 02:17:39 +0300

    Refactor code

--- user-management/user.query.api/src/main/java/com/ats/user/query/api/repositories/UserRepository.java
+++ user-management/user.query.api/src/main/java/com/ats/user/query/api/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.ats.user.query.api.repositories;
 
 import com.ats.user.core.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
